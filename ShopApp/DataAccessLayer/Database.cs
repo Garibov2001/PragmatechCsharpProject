b967b60fc777719844baa66db4344e14010dcf09
Garibov2001 <mahmood.garibov@gmail.com>
2021-01-27 19:31:12 +0400

    User entity and User DAL is done

---
+++ ShopApp/DataAccessLayer/Database.cs
@@ -0,0 +1,42 @@
+﻿using ShopApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccessLayer
+{
+    static class Database
+    {
+        //Database
+        private static List<User> _users;
+        //Static constructor
+        static Database()
+        {
+            _users = new List<User>();
+        }
+
+        public static void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public static void RemoveUser(int id)
+        {
+            var user = _users.SingleOrDefault(x => x.ID == id);
+            _users.Remove(user);
+        }
+
+        public static List<User> GetAll()
+        {
+            return _users;
+        }
+
+        public static User GetById(int id)
+        {
+            return _users.SingleOrDefault(x => x.ID == id); ;
+        }
+
+    }
+}
---
+++ ShopApp/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entity
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int UserName { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+
+    }
+}
