e229c5030b06bf5d4f474e2095ef1b16af19349a
Garibov2001 <mahmood.garibov@gmail.com>
2021-02-01 01:18:02 +0400

    Product delete is done

--- ShopApp/Controls/AccountForm.Designer.cs
+++ ShopApp/Controls/AccountForm.Designer.cs
@@ -30,18 +30,21 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgw_allProducts = new System.Windows.Forms.DataGridView();
             this.pg_myProducts = new System.Windows.Forms.TabPage();
+            this.btn_addProduct = new System.Windows.Forms.Button();
+            this.cmb_filter = new System.Windows.Forms.ComboBox();
+            this.lbl_filter = new System.Windows.Forms.Label();
+            this.txb_search = new System.Windows.Forms.TextBox();
+            this.lbl_search = new System.Windows.Forms.Label();
+            this.dgw_myProducts = new System.Windows.Forms.DataGridView();
             this.lbl_name_surname = new System.Windows.Forms.Label();
             this.lbl_email = new System.Windows.Forms.Label();
             this.lbl_phone = new System.Windows.Forms.Label();
             this.btn_exit = new System.Windows.Forms.Button();
-            this.dgw_myProducts = new System.Windows.Forms.DataGridView();
-            this.lbl_search = new System.Windows.Forms.Label();
-            this.txb_search = new System.Windows.Forms.TextBox();
-            this.lbl_filter = new System.Windows.Forms.Label();
-            this.cmb_filter = new System.Windows.Forms.ComboBox();
-            this.btn_addProduct = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_allProducts)).BeginInit();
             this.pg_myProducts.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgw_myProducts)).BeginInit();
             this.SuspendLayout();
@@ -59,14 +62,23 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Controls.Add(this.dgw_allProducts);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(768, 457);
+            this.tabPage1.Size = new System.Drawing.Size(768, 389);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Bütün məhsullar";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgw_allProducts
+            // 
+            this.dgw_allProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_allProducts.Location = new System.Drawing.Point(24, 83);
+            this.dgw_allProducts.Name = "dgw_allProducts";
+            this.dgw_allProducts.Size = new System.Drawing.Size(717, 150);
+            this.dgw_allProducts.TabIndex = 0;
             // 
             // pg_myProducts
             // 
@@ -83,6 +95,61 @@
             this.pg_myProducts.TabIndex = 1;
             this.pg_myProducts.Text = "Məhsullarım";
             this.pg_myProducts.UseVisualStyleBackColor = true;
+            // 
+            // btn_addProduct
+            // 
+            this.btn_addProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_addProduct.Location = new System.Drawing.Point(601, 308);
+            this.btn_addProduct.Name = "btn_addProduct";
+            this.btn_addProduct.Size = new System.Drawing.Size(142, 39);
+            this.btn_addProduct.TabIndex = 5;
+            this.btn_addProduct.Text = "Məhsul əlavə et";
+            this.btn_addProduct.UseVisualStyleBackColor = true;
+            this.btn_addProduct.Click += new System.EventHandler(this.btn_addProduct_Click);
+            // 
+            // cmb_filter
+            // 
+            this.cmb_filter.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.cmb_filter.FormattingEnabled = true;
+            this.cmb_filter.Location = new System.Drawing.Point(542, 48);
+            this.cmb_filter.Name = "cmb_filter";
+            this.cmb_filter.Size = new System.Drawing.Size(201, 24);
+            this.cmb_filter.TabIndex = 4;
+            // 
+            // lbl_filter
+            // 
+            this.lbl_filter.AutoSize = true;
+            this.lbl_filter.Location = new System.Drawing.Point(507, 16);
+            this.lbl_filter.Name = "lbl_filter";
+            this.lbl_filter.Size = new System.Drawing.Size(236, 17);
+            this.lbl_filter.TabIndex = 3;
+            this.lbl_filter.Text = "Məhsulları kategoriyaya görə filterlə:";
+            // 
+            // txb_search
+            // 
+            this.txb_search.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txb_search.Location = new System.Drawing.Point(20, 48);
+            this.txb_search.Name = "txb_search";
+            this.txb_search.Size = new System.Drawing.Size(169, 23);
+            this.txb_search.TabIndex = 2;
+            // 
+            // lbl_search
+            // 
+            this.lbl_search.AutoSize = true;
+            this.lbl_search.Location = new System.Drawing.Point(17, 16);
+            this.lbl_search.Name = "lbl_search";
+            this.lbl_search.Size = new System.Drawing.Size(214, 17);
+            this.lbl_search.TabIndex = 1;
+            this.lbl_search.Text = "Məhsulun adına görə axtarış et:  ";
+            // 
+            // dgw_myProducts
+            // 
+            this.dgw_myProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_myProducts.Location = new System.Drawing.Point(20, 97);
+            this.dgw_myProducts.Name = "dgw_myProducts";
+            this.dgw_myProducts.Size = new System.Drawing.Size(723, 189);
+            this.dgw_myProducts.TabIndex = 0;
+            this.dgw_myProducts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgw_myProducts_CellClick);
             // 
             // lbl_name_surname
             // 
@@ -124,60 +191,6 @@
             this.btn_exit.Text = "Hesabdan çıx";
             this.btn_exit.UseVisualStyleBackColor = true;
             // 
-            // dgw_myProducts
-            // 
-            this.dgw_myProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgw_myProducts.Location = new System.Drawing.Point(20, 97);
-            this.dgw_myProducts.Name = "dgw_myProducts";
-            this.dgw_myProducts.Size = new System.Drawing.Size(723, 189);
-            this.dgw_myProducts.TabIndex = 0;
-            // 
-            // lbl_search
-            // 
-            this.lbl_search.AutoSize = true;
-            this.lbl_search.Location = new System.Drawing.Point(17, 16);
-            this.lbl_search.Name = "lbl_search";
-            this.lbl_search.Size = new System.Drawing.Size(214, 17);
-            this.lbl_search.TabIndex = 1;
-            this.lbl_search.Text = "Məhsulun adına görə axtarış et:  ";
-            // 
-            // txb_search
-            // 
-            this.txb_search.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.txb_search.Location = new System.Drawing.Point(20, 48);
-            this.txb_search.Name = "txb_search";
-            this.txb_search.Size = new System.Drawing.Size(169, 23);
-            this.txb_search.TabIndex = 2;
-            // 
-            // lbl_filter
-            // 
-            this.lbl_filter.AutoSize = true;
-            this.lbl_filter.Location = new System.Drawing.Point(507, 16);
-            this.lbl_filter.Name = "lbl_filter";
-            this.lbl_filter.Size = new System.Drawing.Size(236, 17);
-            this.lbl_filter.TabIndex = 3;
-            this.lbl_filter.Text = "Məhsulları kategoriyaya görə filterlə:";
-            // 
-            // cmb_filter
-            // 
-            this.cmb_filter.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.cmb_filter.FormattingEnabled = true;
-            this.cmb_filter.Location = new System.Drawing.Point(542, 48);
-            this.cmb_filter.Name = "cmb_filter";
-            this.cmb_filter.Size = new System.Drawing.Size(201, 24);
-            this.cmb_filter.TabIndex = 4;
-            // 
-            // btn_addProduct
-            // 
-            this.btn_addProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_addProduct.Location = new System.Drawing.Point(601, 308);
-            this.btn_addProduct.Name = "btn_addProduct";
-            this.btn_addProduct.Size = new System.Drawing.Size(142, 39);
-            this.btn_addProduct.TabIndex = 5;
-            this.btn_addProduct.Text = "Məhsul əlavə et";
-            this.btn_addProduct.UseVisualStyleBackColor = true;
-            this.btn_addProduct.Click += new System.EventHandler(this.btn_addProduct_Click);
-            // 
             // AccountForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -193,6 +206,8 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AccountForm_FormClosing);
             this.Load += new System.EventHandler(this.AccountForm_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_allProducts)).EndInit();
             this.pg_myProducts.ResumeLayout(false);
             this.pg_myProducts.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgw_myProducts)).EndInit();
@@ -216,5 +231,6 @@
         private System.Windows.Forms.TextBox txb_search;
         private System.Windows.Forms.Label lbl_search;
         private System.Windows.Forms.DataGridView dgw_myProducts;
+        private System.Windows.Forms.DataGridView dgw_allProducts;
     }
 }
--- ShopApp/Controls/AccountForm.cs
+++ ShopApp/Controls/AccountForm.cs
@@ -1,6 +1,8 @@
 ﻿using SalesManagement.Entities.Authentication;
 using ShopApp.Controls.Authentication;
 using ShopApp.Controls.Products;
+using ShopApp.DataAccessLayer;
+using ShopApp.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,13 +17,95 @@ namespace ShopApp.Controls
 {
     public partial class AccountForm : Form
     {
+        private UnitOfWork _unitOfWork;
         private static LoginForm LoginForm { get; set; }
         public static User CurrentUser { get; set; }
         public AccountForm(LoginForm _loginForm)
         {
+            _unitOfWork = new UnitOfWork();
             LoginForm = _loginForm;
-            
-            InitializeComponent();
+            InitializeComponent();            
+        }
+
+        private void LoadAllProducts()
+        {
+            var products = _unitOfWork.Products.GetAll();
+
+            List<dynamic> wholeProducts = new List<dynamic>();
+
+            foreach (var product in products)
+            {
+                wholeProducts.Add(new
+                {
+                    ID = product.ID,
+                    ProductName = product.Name,
+                    Category = _unitOfWork.ProductCategories.Get(x => x.ID == product.ProductCategoryID).Name,
+                    Price = product.Price,
+                    User = _unitOfWork.Users.Get(x => x.ID == product.UserID).Name + " " + _unitOfWork.Users.Get(x => x.ID == product.UserID).Surname,
+                    UserTel = _unitOfWork.Users.Get(x => x.ID == product.UserID).PhoneNumber,
+                    Count = product.Count,
+                    Status = (ProductStatus)product.ProductStatus,
+                });
+            }
+            // Hide the id in the grid
+
+            dgw_allProducts.Columns.Clear();
+            dgw_allProducts.DataSource = wholeProducts;
+
+            //Because of out of index exception
+            if (dgw_allProducts.Columns.Count > 0)
+            {
+                dgw_allProducts.Columns[0].Visible = false;
+
+            }
+        }
+
+        private void LoadPersonalProducts()
+        {
+            var products = _unitOfWork.Products.GetAll(x => x.UserID == CurrentUser.ID);
+
+            List<dynamic> wholeProducts = new List<dynamic>();
+
+            foreach (var product in products)
+            {
+                wholeProducts.Add(new
+                {
+                    ID = product.ID,
+                    ProductName = product.Name,
+                    Category = _unitOfWork.ProductCategories.Get(x => x.ID == product.ProductCategoryID).Name,
+                    Price = product.Price,
+                    Count = product.Count,
+                    Status = (ProductStatus)product.ProductStatus,
+                });
+            }
+
+            // Hide the id in the grid
+            dgw_myProducts.Columns.Clear();
+            dgw_myProducts.DataSource = wholeProducts;
+
+            //Because of out of index exception
+            if (dgw_myProducts.Columns.Count > 0)
+            {
+                dgw_myProducts.Columns[0].Visible = false;
+
+                //Edit Button:
+                DataGridViewButtonColumn editBtn = new DataGridViewButtonColumn();
+                editBtn.HeaderText = "Editlemek";
+                editBtn.Name = "Editlemek";
+                editBtn.Text = "Edit";
+                editBtn.UseColumnTextForButtonValue = true;
+
+                dgw_myProducts.Columns.Add(editBtn);
+                //Remove Button:
+                DataGridViewButtonColumn deleteBtn = new DataGridViewButtonColumn();
+                deleteBtn.HeaderText = "Silmek";
+                deleteBtn.Name = "Silmek";
+                deleteBtn.Text = "Sil";
+                deleteBtn.UseColumnTextForButtonValue = true;
+
+                dgw_myProducts.Columns.Add(deleteBtn);
+
+            }
         }
 
         private void AccountForm_Load(object sender, EventArgs e)
@@ -30,6 +114,8 @@ namespace ShopApp.Controls
             lbl_name_surname.Text += CurrentUser.Name + " " + CurrentUser.Surname;
             lbl_email.Text += CurrentUser.Email;
             lbl_phone.Text += CurrentUser.PhoneNumber;
+            LoadAllProducts();
+            LoadPersonalProducts();
         }
 
         private void AccountForm_FormClosing(object sender, FormClosingEventArgs e)
@@ -41,6 +127,42 @@ namespace ShopApp.Controls
         {
             var addProductForm = new AddProductForm();
             addProductForm.ShowDialog();
+            LoadAllProducts();
+            LoadPersonalProducts();
         }
+
+        private void dgw_myProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // 0 - edit (because of datagrid bug i have to write or condition)
+            if (e.ColumnIndex == 0 || e.ColumnIndex == dgw_myProducts.Columns.Count - 2)
+            {
+            }
+            // 1 - remove
+            else if (e.ColumnIndex == 1 || e.ColumnIndex == dgw_myProducts.Columns.Count - 1)
+            {
+                var result = MessageBox.Show("Bunu etmek istediyinden eminsen?", "Mehsulu silmek", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    foreach (DataGridViewCell cell in dgw_myProducts.CurrentRow.Cells) 
+                    {
+                        string name = dgw_myProducts.Columns[cell.ColumnIndex].Name;
+                        if (name == "ID")
+                        {
+                            var productID = (int)cell.Value;
+                            var product = _unitOfWork.Products.Get(x => x.ID == productID);
+                            _unitOfWork.Products.Delete(product);
+                            break;
+                        }
+                    }                                            
+                }
+            }                           
+
+            LoadAllProducts();
+            LoadPersonalProducts();
+        }
+
+
+
     }
 }
